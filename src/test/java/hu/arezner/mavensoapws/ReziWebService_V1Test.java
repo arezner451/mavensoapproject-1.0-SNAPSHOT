ee5e8dc6282972f09170827ebcf49b538d0a92ac
arezner451 <attila.rezner@gmail.com>
2020-05-03 17:05:56 +0200

    added test.

---
+++ src/test/java/hu/arezner/mavensoapws/ReziWebService_V1Test.java
@@ -0,0 +1,26 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ati <attila.rezner@gmail.com>
+ * @created 2020.05.03., 16:52:02
+ */
+public class ReziWebService_V1Test {
+
+    private static final Logger LOG = Logger.getLogger(ReziWebService_V1Test.class.getName());
+    
+    @Test
+    public void checkWSOffline() {
+        LOG.log(Level.INFO, "test running...");
+    }    
+    
+}
